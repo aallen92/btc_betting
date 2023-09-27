cffa89420ade9d779a9f6f299094b55e4ce52373
najamzozo <najam457@gmail.com>
2023-09-28 01:29:40 +0500

    exchange api

--- flick-the-bean/src/components/recent-flickers-table/recentFlickerTable.tsx
+++ flick-the-bean/src/components/recent-flickers-table/recentFlickerTable.tsx
@@ -9,16 +9,17 @@ interface tableData {
 
 interface RecentFlickersTableProps {
   tableData: Array<tableData>;
+  classname?: string;
 }
 
-const RecentFlickersTable:FC<RecentFlickersTableProps> = ({ tableData }) => {
+const RecentFlickersTable:FC<RecentFlickersTableProps> = ({ tableData, classname }) => {
   return(
-    <ul className="primary-list primary-list--home">
+    <ul className={`primary-list primary-list--${classname ? classname : 'home'}`}>
       {
         tableData ? tableData.map((item, index) => (
           <li className="primary-list__item" key={index}>
             <div className="primary-list__col">{item?.public_key.slice(0, 5)}...{item?.public_key.slice(-5)}</div>
-            <div className="primary-list__col-2">flipped {Math.round((parseInt(item.bet_amount) + Number.EPSILON) * 100) / 100} ACD3 and {item.outcome}.</div>
+            <div className="primary-list__col-2">flipped {Math.round((parseFloat(item.bet_amount) + Number.EPSILON) * 100) / 100} ACD3 and {item.outcome}.</div>
             <div className="primary-list__col">{item.timeAgo}</div>
           </li>
         )) : <p style={{ textAlign: 'center', marginTop: 50 }}>Loading Data</p>
