c32251a42427c430b763e791888771d2de301e80
vadym110 <koval.43vadym3@hotmail.com>
2023-10-04 09:09:43 -0400

    Add action in no wallet page

--- flick-the-bean/src/components/home-content/homeContent.tsx
+++ flick-the-bean/src/components/home-content/homeContent.tsx
@@ -47,7 +47,7 @@ const HomeContent:FC = () => {
           <span>
             #1 place to <br /> bean flick and <br /> coin flip
           </span>
-          <img src="/static/img/start_button.png" alt="start_button"/>
+          <img src="/static/img/start_button.png" alt="start_button" onClick={handleModal}/>
           <RecentFlickersTable tableData={data} />
         </section>
       </div>
