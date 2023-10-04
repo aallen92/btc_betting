0190e98683234fb99b5999c0ff3a0cd1a1b6344e
vadym110 <koval.43vadym3@hotmail.com>
2023-10-04 09:07:35 -0400

    Complete no wallet page

--- flick-the-bean/src/components/home-content/homeContent.tsx
+++ flick-the-bean/src/components/home-content/homeContent.tsx
@@ -37,24 +37,20 @@ const HomeContent:FC = () => {
   }
   return(
     <div>
-      <section>
-        <h1 className="heading-primary">
-          #1 PLACE TO <br /><span className="heading-primary__thick">BEAN</span> FLICK AND
-          <span className="heading-primary__thick"> COIN</span> FLIP
-        </h1>
-      </section>
-      <section className="top-area">
-        <button className="primary-btn" onClick={handleModal}>Play</button>
-        <div className="arrow-down">
-          <img className="arrow-down__figure" src="/static/img/arrow.svg" alt="arrow down icon" />
-        </div>
-      </section>
-      <section className="center-area">
-        <div className="line"></div>
-        <h2 className="main__heading">MOST RECENT BEAN FLICKERS</h2>
-        <RecentFlickersTable tableData={data} />
-        <h2 className="secondary-heading">Flip Responsibly!</h2>
-      </section>
+      <div className="home-content">
+        <section>
+          <h1 className="heading-primary">
+            <img src="/static/img/landing.png" alt="landing"/>
+          </h1>
+        </section>
+        <section className="center-area">
+          <span>
+            #1 place to <br /> bean flick and <br /> coin flip
+          </span>
+          <img src="/static/img/start_button.png" alt="start_button"/>
+          <RecentFlickersTable tableData={data} />
+        </section>
+      </div>
       <PlayModal show={showModal} handleModal={handleModal} />
     </div>
   )
--- flick-the-bean/src/components/recent-flickers-table/recentFlickerTable.tsx
+++ flick-the-bean/src/components/recent-flickers-table/recentFlickerTable.tsx
@@ -15,15 +15,26 @@ interface RecentFlickersTableProps {
 const RecentFlickersTable:FC<RecentFlickersTableProps> = ({ tableData, classname }) => {
   return(
     <ul className={`primary-list primary-list--${classname ? classname : 'home'}`}>
-      {
-        tableData ? tableData.map((item, index) => (
-          <li className="primary-list__item" key={index}>
-            <div className="primary-list__col">{item?.public_key.slice(0, 5)}...{item?.public_key.slice(-5)}</div>
-            <div className="primary-list__col-2">flipped {Math.round((parseFloat(item.bet_amount) + Number.EPSILON) * 100) / 100} ACD3 and {item.outcome}.</div>
-            <div className="primary-list__col">{item.timeAgo}</div>
-          </li>
-        )) : <p style={{ textAlign: 'center', marginTop: 50 }}>Loading Data</p>
-      }
+      <li className="primary-list__header">
+        <div className="primary-list__header__col">Most recent been flickers</div>
+        <div className="primary-list__header__col-2">See all</div>
+      </li>
+      <li>
+        <ul>
+        {
+          tableData ? tableData.map((item, index) => (
+            <li className="primary-list__item" key={index}>
+              <div className="primary-list__col">{item?.public_key.slice(0, 5)}...{item?.public_key.slice(-5)}</div>
+              <div className="primary-list__col-2">flipped <span>{Math.round((parseFloat(item.bet_amount) + Number.EPSILON) * 100) / 100} ACD3</span> and <span style={{color: item.outcome == 'lost' && '#EF4343'}}>{item.outcome}</span></div>
+              <div className="primary-list__col-3">{item.timeAgo}</div>
+            </li>
+          )) : <p style={{ textAlign: 'center', marginTop: 50 }}>Loading Data</p>
+        }  
+        </ul>  
+      </li>
+      
+      <div>
+      </div>
     </ul>
   )
 }
