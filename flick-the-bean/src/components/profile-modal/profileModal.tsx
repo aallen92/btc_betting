35264e6c8deeb31a2b3b98c54114da9fabdf29e4
vadym110 <koval.43vadym3@hotmail.com>
2023-10-10 17:55:30 -0400

    Save profile page

--- flick-the-bean/src/components/profile-modal/profileModal.tsx
+++ flick-the-bean/src/components/profile-modal/profileModal.tsx
@@ -34,7 +34,147 @@ const ProfileModal:FC<ProfileModalProps> = ({ show, handleModal }) => {
   return(
 		<Modal customClass={'profile-modal'} show={show} handleModal={handleModal}>
 			<div className="profile">
-				
+				<div className="profile-header">
+					<div className="profile-header-left">
+						<img src={"/static/img/avatar.png"} />
+						<div>
+							<span>
+								David <br /> Copperfield
+							</span>
+							<span>
+								0x71C.....765483920
+							</span>
+							<span>
+								Flipping since Sep 2023
+							</span>
+						</div>
+					</div>
+					<div className="profile-header-right">
+						<div>
+							<span>
+								Current rank
+							</span>
+							<span>
+								14
+							</span>
+						</div>
+						<div>
+							<span>
+								Highest
+							</span>
+							<span>
+								2
+							</span>
+						</div>
+					</div>
+				</div>
+				<div className="profile-value">
+					<div className="profile-value-item">
+						<span>
+							Success in a row
+						</span>
+						<span>
+							10
+						</span>
+					</div>
+					<div className="profile-value-item">
+						<span>
+							Success in a row
+						</span>
+						<span>
+							10
+						</span>
+					</div>
+					<div className="profile-value-item">
+						<span>
+							Success in a row
+						</span>
+						<span>
+							10
+						</span>
+					</div>
+					<div className="profile-value-item">
+						<span>
+							Success in a row
+						</span>
+						<span>
+							10
+						</span>
+					</div>
+					<div className="profile-value-item">
+						<span>
+							Success in a row
+						</span>
+						<span>
+							10
+						</span>
+					</div>
+					<div className="profile-value-item">
+						<span>
+							Success in a row
+						</span>
+						<span>
+							10
+						</span>
+					</div>
+					<div className="profile-value-item">
+						<span>
+							Success in a row
+						</span>
+						<span>
+							10
+						</span>
+					</div>
+					<div className="profile-value-item">
+						<span>
+							Success in a row
+						</span>
+						<span>
+							10
+						</span>
+					</div>
+					
+				</div>
+				<div className="profile-other">
+					<div className="history">
+						<div className="title">
+							Flip history
+						</div>
+						<div className="content">
+							<div className="row">
+								<div className="desc">
+									flipped 0.1 eth and won
+								</div>
+								<div className="time">
+									1h
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="referrals">
+						<div className="title">
+							Referrals
+						</div>
+						<div className="content">
+							<div className="code">
+
+							</div>
+							<div className="statistics">
+								<div className="user_number">
+									8
+								</div>
+								<div className="total_earned">
+									24
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="badges">
+						<div className="title">
+							Badges
+						</div>
+					</div>
+				</div>
 			</div>
 		</Modal>
   )
